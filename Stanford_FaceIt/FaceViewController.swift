cb3f25449e9294662ad03342c2b676d3ff671d0f
Phil <pblanchette44@hotmail.com>
2017-02-11 11:47:03 -0500

    cxcxc

---
+++ Stanford_FaceIt/FaceViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Stanford_FaceIt
+//
+//  Created by Philippe Blanchette on 2017-02-11.
+//  Copyright © 2017 Philippe Blanchette. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+    
+}
+
