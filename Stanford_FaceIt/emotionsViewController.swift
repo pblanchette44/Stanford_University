dd523841b94e6af51da655e27d6319cb751dc549
Phil <pblanchette44@hotmail.com>
2017-02-12 17:24:40 -0500

    Added second View for emotion control
    Added Tabbed View Controller for navigation between the two view controller

---
+++ Stanford_FaceIt/emotionsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  emotionsViewController.swift
+//  Stanford_FaceIt
+//
+//  Created by Philippe Blanchette on 2017-02-12.
+//  Copyright © 2017 Philippe Blanchette. All rights reserved.
+//
+
+import UIKit
+
+class emotionsViewController: UIViewController {
+
+  
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
