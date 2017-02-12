ae01de3b8049011adbc4fd2d93f7301ee79ad61f
Phil <pblanchette44@hotmail.com>
2017-02-12 12:35:27 -0500

    connected the FaceViewcontroller to the model and the faceView

--- Stanford_FaceIt/FaceViewController.swift
+++ Stanford_FaceIt/FaceViewController.swift
@@ -10,6 +10,42 @@ import UIKit
 
 class FaceViewController: UIViewController {
 
+    var expression: facialExpression = facialExpression(
+        eyes: .Open,
+        eyeBrows: .Normal,
+        mouth: .Smirk
+    ) {didSet{updateUI()}}
+    
+    
+    @IBOutlet var faceView: faceView! {
+        didSet{
+            updateUI()
+        }
+    }
+    
+    
+    //Mouth dictionnary
+    private var mouthCurvatures = [
+    facialExpression.Mouth.Frown: -1.0,
+    .Grin:0.5,
+    .Smile:1.0,
+    .Smirk:-0.5,
+    .Neutral:0.0]
+    
+    
+    private func updateUI(){
+        switch expression.eyes{
+            case .Open:
+                faceView.eyesOpen = true
+            case .Closed:
+                faceView.eyesOpen = false
+            case .Squinting:
+                faceView.eyesOpen = false
+        }
+        faceView.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
+    }
+    
+    
     
     
 }
